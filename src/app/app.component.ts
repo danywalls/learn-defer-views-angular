55231eab15dd536dcf05358e63168ba061f418ba
Dany Paredes <danywalls4@gmail.com>
2024-04-13 12:03:53 +0200

    feat: 🎸 initial stage
    all components are load by default
    
    BREAKING CHANGE: 🧨 N/A

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {JumpComponent} from "./components/jump.component";
+import {LetterComponent} from "./components/letter.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JumpComponent, LetterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'defer_gray';
+  jumpAccept: boolean = false;
 }
---
+++ src/app/components/jump.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'jump',
+  template: `
+    <section class="bg-white rounded-lg shadow px-8 py-5 mb-10">
+      <h2 class="text-xl text-center font-semibold p-5">Welcome to Red Side!!!</h2>
+      <img src="assets/developer.webp" alt="Logo" >
+
+    </section>
+
+  `,
+  standalone: true
+})
+
+export class JumpComponent  {
+
+}
